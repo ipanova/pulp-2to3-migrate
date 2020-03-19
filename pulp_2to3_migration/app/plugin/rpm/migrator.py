987dc480ede0135076f0ef904f24417a772eb85f
Ina Panova <ipanova@redhat.com>
2020-03-19 16:25:05 +0100

    Migrate comps.
    [noissue]

--- pulp_2to3_migration/app/plugin/rpm/migrator.py
+++ pulp_2to3_migration/app/plugin/rpm/migrator.py
@@ -1,3 +1,9 @@
+import functools
+import logging
+
+from gettext import gettext as _
+
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import transaction
 from django.db.models import Q
 
@@ -16,6 +22,10 @@ from .pulp2_models import (
     Errata,
     Modulemd,
     ModulemdDefaults,
+    PackageCategory,
+    PackageGroup,
+    PackageEnvironment,
+    PackageLangpacks,
     RPM,
     YumMetadataFile,
 )
@@ -23,6 +33,10 @@ from .pulp_2to3_models import (
     Pulp2Erratum,
     Pulp2Modulemd,
     Pulp2ModulemdDefaults,
+    Pulp2PackageCategory,
+    Pulp2PackageEnvironment,
+    Pulp2PackageGroup,
+    Pulp2PackageLangpacks,
     Pulp2Rpm,
     Pulp2YumRepoMetadataFile,
 )
@@ -34,6 +48,8 @@ from .repository import (
 from pulpcore.plugin.stages import (
     ArtifactSaver,
     ContentSaver,
+    DeclarativeArtifact,
+    DeclarativeContent,
     RemoteArtifactSaver,
     ResolveContentFutures,
     Stage,
@@ -42,6 +58,14 @@ from pulpcore.plugin.stages import (
 )
 
 from . import package_utils
+from pulp_2to3_migration.app.constants import NOT_USED
+from pulp_2to3_migration.app.models import (
+    Pulp2Importer,
+    Pulp2LazyCatalog,
+)
+
+
+_logger = logging.getLogger(__name__)
 
 
 class RpmMigrator(Pulp2to3PluginMigrator):
@@ -65,6 +89,10 @@ class RpmMigrator(Pulp2to3PluginMigrator):
         'modulemd': Modulemd,
         'modulemd_defaults': ModulemdDefaults,
         'yum_repo_metadata_file': YumMetadataFile,
+        'package_langpacks': PackageLangpacks,
+        'package_group': PackageGroup,
+        'package_category': PackageCategory,
+        'package_environment': PackageEnvironment,
     }
     pulp2_collection = 'units_rpm'
     pulp3_plugin = 'pulp_rpm'
@@ -75,6 +103,10 @@ class RpmMigrator(Pulp2to3PluginMigrator):
         ('modulemd', Pulp2Modulemd),
         ('modulemd_defaults', Pulp2ModulemdDefaults),
         ('yum_repo_metadata_file', Pulp2YumRepoMetadataFile),
+        ('package_langpacks', Pulp2PackageLangpacks),
+        ('package_group', Pulp2PackageGroup),
+        ('package_category', Pulp2PackageCategory),
+        ('package_environment', Pulp2PackageEnvironment),
     ])
     mutable_content_models = {
         'erratum': Pulp2Erratum,
@@ -99,7 +131,7 @@ class RpmMigrator(Pulp2to3PluginMigrator):
         """
         Migrate pre-migrated Pulp 2 RPM plugin content.
         """
-        first_stage = ContentMigrationFirstStage(cls)
+        first_stage = RpmContentMigrationFirstStage(cls)
         dm = RpmDeclarativeContentMigration(first_stage=first_stage)
         await dm.create()
 
@@ -132,6 +164,108 @@ class RpmDeclarativeContentMigration(DeclarativeContentMigration):
         ]
 
         return pipeline
+
+
+class RpmContentMigrationFirstStage(ContentMigrationFirstStage):
+    """
+    The first stage of a Rpm content migration pipeline.
+    """
+
+    async def migrate_to_pulp3(self, batch, pb=None):
+        """
+        A default implementation of DeclarativeContent creation for migrating content to Pulp 3.
+
+        Plugin writers might want to override this method if it doesn't satisfy their needs as is.
+
+        In this implementation there is an assumption that each content has one artifact.
+
+        Args:
+            batch: A batch of Pulp2Content objects to migrate to Pulp 3
+        """
+        @functools.lru_cache(maxsize=20)
+        def get_remote_by_importer_id(importer_id):
+            """
+            Args:
+                importer_id(str): Id of an importer in Pulp 2
+
+            Returns:
+                remote(pulpcore.app.models.Remote): A corresponding remote in Pulp 3
+
+            """
+            try:
+                pulp2importer = Pulp2Importer.objects.get(pulp2_object_id=importer_id)
+            except ObjectDoesNotExist:
+                return
+            return pulp2importer.pulp3_remote
+
+        for pulp_2to3_detail_content in batch:
+            pulp2content = pulp_2to3_detail_content.pulp2content
+
+            if getattr(pulp2content, 'downloaded'):
+
+                # get all Lazy Catalog Entries (LCEs) for this content
+                pulp2lazycatalog = Pulp2LazyCatalog.objects.filter(
+                    pulp2_unit_id=pulp2content.pulp2_id)
+
+                if not pulp2content.downloaded and not pulp2lazycatalog:
+                    _logger.warn(_('On_demand content cannot be migrated without an entry in the '
+                                   'lazy catalog, pulp2 unit_id: '
+                                   '{}'.format(pulp2content.pulp2_id)))
+                    continue
+
+            pulp3content = await pulp_2to3_detail_content.create_pulp3_content()
+            future_relations = {'pulp2content': pulp2content}
+
+            comps_types = ('package_langpacks', 'package_group', 'package_category',
+                           'package_environment')
+            if pulp_2to3_detail_content.pulp2_type in comps_types:
+                # dc without artifact
+                dc = DeclarativeContent(content=pulp3content)
+                dc.extra_data = future_relations
+                await self.put(dc)
+            else:
+
+                artifact = await self.create_artifact(pulp2content.pulp2_storage_path,
+                                                      pulp_2to3_detail_content.expected_digests,
+                                                      pulp_2to3_detail_content.expected_size,
+                                                      downloaded=pulp2content.downloaded)
+
+                # Downloaded content with no LCE
+                if pulp2content.downloaded and not pulp2lazycatalog:
+                    da = DeclarativeArtifact(
+                        artifact=artifact,
+                        url=NOT_USED,
+                        relative_path=pulp_2to3_detail_content.relative_path_for_content_artifact,
+                        remote=None,
+                        deferred_download=False)
+                    dc = DeclarativeContent(content=pulp3content, d_artifacts=[da])
+                    dc.extra_data = future_relations
+                    await self.put(dc)
+
+                # Downloaded or on_demand content with LCEs.
+                #
+                # To create multiple remote artifacts, create multiple instances of declarative
+                # content which will differ by url/remote in their declarative artifacts
+                for lce in pulp2lazycatalog:
+                    remote = get_remote_by_importer_id(lce.pulp2_importer_id)
+                    deferred_download = not pulp2content.downloaded
+                    if not remote and deferred_download:
+                        _logger.warn(_('On_demand content cannot be migrated without a remote '
+                                       'pulp2 unit_id: {}'.format(pulp2content.pulp2_id)))
+                        continue
+
+                    da = DeclarativeArtifact(
+                        artifact=artifact,
+                        url=lce.pulp2_url,
+                        relative_path=pulp_2to3_detail_content.relative_path_for_content_artifact,
+                        remote=remote,
+                        deferred_download=deferred_download)
+                    dc = DeclarativeContent(content=pulp3content, d_artifacts=[da])
+                    dc.extra_data = future_relations
+                    await self.put(dc)
+
+            if pb:
+                pb.increment()
 
 
 class InterrelateContent(Stage):
